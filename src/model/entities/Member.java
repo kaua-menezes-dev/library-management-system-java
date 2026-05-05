30412da58d294e820f91677a5b135f4046c386df
Kauã Menezes <kauavictor.f.menezes@hotmail.com>
2026-05-05 16:27:39 -0300

    feat: implementação da classe Member (responsavel por armazenar os dados de um membro)

---
+++ src/model/entities/Member.java
@@ -0,0 +1,62 @@
+package model.entities;
+
+import model.enums.MemberStatus;
+
+public class Member {
+
+    private String name;
+    private String email;
+    private String membershipId;
+    private MemberStatus memberStatus;
+
+    public Member(){}
+
+    public Member(String name, String email, String membershipId) {
+        this.name = name;
+        this.email = email;
+        this.membershipId = membershipId;
+        this.memberStatus = MemberStatus.ACTIVE;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMembershipId() {
+        return membershipId;
+    }
+
+    public void setMembershipId(String membershipId) {
+        this.membershipId = membershipId;
+    }
+
+    public MemberStatus getMemberStatus() {
+        return memberStatus;
+    }
+
+    public void setMemberStatus(MemberStatus memberStatus) {
+        this.memberStatus = memberStatus;
+    }
+
+    public boolean isSuspended(){
+
+        if (memberStatus == MemberStatus.ACTIVE) {
+            return true;
+        }
+
+        return false;
+
+    }
+}
