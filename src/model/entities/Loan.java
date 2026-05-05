8dcb16e8ca7e55c6f88897ee7927233bef09f651
Kauã Menezes <kauavictor.f.menezes@hotmail.com>
2026-05-05 17:23:07 -0300

    feat: implementação da classe Loan (responsavel por implementar os dados de emprestimo + validações do emprestimo)

---
+++ src/model/entities/Loan.java
@@ -0,0 +1,94 @@
+package model.entities;
+
+import model.enums.MemberStatus;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class Loan {
+
+    private LocalDate loanDate;
+    private LocalDate expectedReturnDate;
+    private LocalDate actualReturnDate;
+    private Double fee;
+    private Book book;
+    private Member member;
+
+    public Loan() {
+    }
+
+    public Loan(Book book, Member member) {
+        this.book = book;
+        this.member = member;
+        this.loanDate = LocalDate.now();
+        this.expectedReturnDate = loanDate.plusDays(14);
+        this.fee = 0.0;
+    }
+
+    public LocalDate getLoanDate() {
+        return loanDate;
+    }
+
+    public LocalDate getExpectedReturnDate() {
+        return expectedReturnDate;
+    }
+
+    public LocalDate getActualReturnDate() {
+        return actualReturnDate;
+    }
+
+    public Double getFee() {
+        return fee;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setActualReturnDate(LocalDate actualReturnDate) {
+        this.actualReturnDate = actualReturnDate;
+    }
+
+    public void calculatedFee() {
+
+        long days = ChronoUnit.DAYS.between(expectedReturnDate, actualReturnDate);
+
+        if (days >= 14) {
+            this.fee = days * 2.0;
+        }
+
+    }
+
+    public boolean isLate() {
+
+        if (actualReturnDate == null) {
+            return false;
+        }
+
+        return actualReturnDate.isAfter(expectedReturnDate);
+
+    }
+
+    public long getDelayDays() {
+
+        if (actualReturnDate == null || !isLate()) {
+            return 0;
+        }
+
+        return ChronoUnit.DAYS.between(expectedReturnDate, actualReturnDate);
+
+    }
+
+    public boolean isReturned() {
+        return actualReturnDate != null;
+    }
+
+    public boolean isActive() {
+        return !isReturned();
+    }
+
+}
